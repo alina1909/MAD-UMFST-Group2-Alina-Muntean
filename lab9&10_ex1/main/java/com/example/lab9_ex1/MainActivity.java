9b82bcf54b6a5a328a997632e47cd96385d7899d
alina1909 <145763382+alina1909@users.noreply.github.com>
2024-03-27 20:28:51 +0200

    Add files via upload

---
+++ lab9&10_ex1/main/java/com/example/lab9_ex1/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.lab9_ex1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import org.json.JSONException;
+import org.json.JSONObject;
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView rezultat = findViewById(R.id.textView1);
+        OkHttpClient client = new OkHttpClient();
+
+        String url = "https://reqres.in/api/users?page=2";
+
+
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                if (response.isSuccessful()) {
+                    final String myResponse = response.body().string();
+                    MainActivity.this.runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            rezultat.setText(myResponse);
+                        }
+                    });
+
+                }
+            }
+        });
+
+    }
+}
